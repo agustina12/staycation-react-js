8a0274d366a6cb7d37fe54c7d01ac3c7d65eb55d
agustina <gustina200@gmail.com>
2024-10-30 15:47:27 +0800

    Finish testing on component Button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -13,6 +13,7 @@ Button.propTypes = {
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
   isPrimary: propTypes.bool,
+  isLight: propTypes.bool,
   isExternal: propTypes.bool,
   hasShadow: propTypes.bool,
 };
@@ -21,6 +22,7 @@ function Button(props) {
   //logic untuk className aktivitas button dari bootstrap-nya
   const className = [props.className];
   if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
   if (props.isLarge) className.push("btn-lg");
   if (props.isSmall) className.push("btn-sm");
   if (props.isBlock) className.push("btn-block");
@@ -36,7 +38,6 @@ function Button(props) {
     if (props.isDisabled) className.push("disabled");
     return (
       <span className={className.join(" ")} style={props.style}>
-        {" "}
         {props.isLoading ? (
           <>
             <span className="spinner-border spinner-border-sm mx-5"></span>
@@ -49,18 +50,44 @@ function Button(props) {
     );
   }
 
-  //button-nya akan mengarah ke link external
-  if(props.type==="link"){
-    if(props.isExternal){
-      return(
-        <a href={props.href} className={className.join(" ")} style={props.style} target={}></a>
-        
-      )
+  //button-nya akan mengarah ke link external atau internal
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      return (
+        <Link
+          to={props.href}
+          className={className.join("")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
     }
   }
 
-  //
-  return <div>index</div>;
+  //mengembalikan button
+  return (
+    <button
+      className={className.join("")}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
